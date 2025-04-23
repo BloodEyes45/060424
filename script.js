f7961b9ed4d9e1a260ac798ae690708b429edd6d
BloodEyes45 <garenimsilik45@gmail.com>
2025-04-23 22:29:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// 💖 Kalbe tıklayınca rastgele renk değiştir
-const heart = document.querySelector('.heart');
-
-heart.addEventListener('click', () => {
-  const colors = ['#ff5c8d', '#ff3b58', '#ff9a8b', '#f1c4c1', '#ff69b4', '#e75480'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  heart.style.backgroundColor = randomColor;
-});
+// script.js
+document.body.style.transition = "background-color 1s ease";
+document.body.style.backgroundColor = "#1c1c1c";

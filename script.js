d79913f770999652a6bd660b4616f643e76735e3
BloodEyes45 <garenimsilik45@gmail.com>
2025-04-23 21:41:42 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const greeting = document.querySelector(".greeting");
+  setTimeout(() => {
+    greeting.innerHTML += " 🌙";
+  }, 4000); // "İyi Geceler" yazısına gece simgesi ekleme
+});
